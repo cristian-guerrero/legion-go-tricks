eac3e320cfd9dd32e2989a069f643a43ddc5e13a
Aarron Lee <aclee91@gmail.com>
2023-12-08 00:54:09 -0500

    add bazzite install script

---
+++ lgo_bazzite_setup.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+# Lenovo Legion Go Bazzite setup script
+# does the following:
+# - basic TDP control via SimpleDeckyTDP plugin
+# - RGB control via LegionGoRemapper Decky Plugin
+
+BUILD_DIR="/tmp/LGO_bazzite_setup"
+
+# Define the user's home directory
+# This ensures that we get the original user's home directory, not root's
+if [ "$SUDO_USER" ]; then
+    HOME_DIR=$(getent passwd "$SUDO_USER" | cut -d: -f6)
+else
+    HOME_DIR=$HOME
+fi
+
+# Ensure running as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root." >&2
+    exit 1
+fi
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+echo "installing SimpleDeckyTDP plugin"
+# download + install Simple TDP Decky Plugin
+curl -L $(curl -s https://api.github.com/repos/aarron-lee/SimpleDeckyTDP/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $BUILD_DIR/SimpleDeckyTDP.tar.gz
+tar -xzf SimpleDeckyTDP.tar.gz -C $HOME_DIR/homebrew/plugins
+
+echo "installing LegionGoRemapper plugin for RGB control"
+# download + install Legion go remapper
+curl -L $(curl -s https://api.github.com/repos/aarron-lee/LegionGoRemapper/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $BUILD_DIR/LegionGoRemapper.tar.gz
+tar -xzf LegionGoRemapper.tar.gz -C $HOME_DIR/homebrew/plugins
+
+cd $BUILD_DIR
+
+# install complete, remove build dir
+cd /
+rm -rf $BUILD_DIR
+echo "Installation complete"
