6ff5095583046fab3c952d4d73ad8c5d804ad4d8
Aarron Lee <aclee91@gmail.com>
2023-12-24 05:41:33 -0500

    update disable_refresh_rates

--- disable_refresh_rates.sh
+++ disable_refresh_rates.sh
@@ -3,4 +3,10 @@
 echo "this is no longer necessary, see updated instructions on the legion-go-tricks website"
 
 mkdir -p $HOME/.config/environment.d
-rm $HOME/.config/environment.d/disable-refresh-rates.conf
+
+rm -f $HOME/.config/environment.d/disable-refresh-rates.conf
+
+cat <<EOF > "$HOME/.config/environment.d/override-gamescopecmd.conf"
+export GAMESCOPECMD="\$GAMESCOPECMD -r 60 "
+export STEAM_DISPLAY_REFRESH_LIMITS=""
+EOF
