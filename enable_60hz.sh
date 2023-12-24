26936065edab9395d9c099d4b928d66dc4ff3337
Aarron Lee <aclee91@gmail.com>
2023-12-23 22:34:56 -0500

    suppress failed-to-find-file error

--- enable_60hz.sh
+++ enable_60hz.sh
@@ -3,7 +3,7 @@
 echo "Enabling 60Hz in Game Mode"
 
 mkdir -p $HOME/.config/environment.d
-rm $HOME/.config/environment.d/disable-refresh-rates.conf
+rm -f $HOME/.config/environment.d/disable-refresh-rates.conf
 
 cat <<EOF > "$HOME/.config/environment.d/override-gamescopecmd.conf"
 export GAMESCOPECMD="\$GAMESCOPECMD -r 60 "
