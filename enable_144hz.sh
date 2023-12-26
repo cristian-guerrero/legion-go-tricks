167f805050603b786faa99993482b1cdabc2a45e
Aarron Lee <aclee91@gmail.com>
2023-12-26 17:30:55 -0500

    update 144hz script

--- enable_144hz.sh
+++ enable_144hz.sh
@@ -7,7 +7,7 @@ rm -f $HOME/.config/environment.d/disable-refresh-rates.conf
 
 cat <<EOF > "$HOME/.config/environment.d/override-gamescopecmd.conf"
 export GAMESCOPECMD="\$GAMESCOPECMD -r 144 "
-export STEAM_DISPLAY_REFRESH_LIMITS="144,144"
+export STEAM_DISPLAY_REFRESH_LIMITS="60,144"
 EOF
 
 echo "Done. Please restart Steam Game Mode to see changes"
