88859bd7e38b22f8aea2d7b2c6ede3125a40d686
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-01-11 21:31:12 -0500

    add script for return to desktop

---
+++ add_return_to_desktop.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+echo 'adding return to desktop icon'
+
+if [ "$(id -u)" -e 0 ]; then
+    echo "This script must be not be run as root, don't use sudo" >&2
+    exit 1
+fi
+
+
+cat <<EOF >> "$HOME/Desktop/return.desktop"
+[Desktop Entry]
+Name=Return to Gaming Mode
+Exec=steamos-desktop-return
+Icon=steamdeck-gaming-return
+Terminal=false
+Type=Application
+StartupNotify=false
+EOF
+
+chmod +x $HOME/Desktop/return.desktop
+
+echo "done!"
