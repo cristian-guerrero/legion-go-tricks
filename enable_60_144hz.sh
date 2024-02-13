815158a6f0f7902ff1e306eadf800a5247ca9560
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-02-12 20:49:18 -0500

    maybe fix script

--- enable_60_144hz.sh
+++ enable_60_144hz.sh
@@ -11,7 +11,7 @@ sudo rm -rf $HOME/homebrew/plugins/LegionGoRefreshRate
 
 cat <<EOF > "$HOME/.config/environment.d/override-gamescopecmd.conf"
 export ENABLE_GAMESCOPE_WSI=1
-export STEAM_DISPLAY_REFRESH_LIMITS="60,144"
+export STEAM_DISPLAY_REFRESH_LIMITS=60,144
 EOF
 
 sudo systemctl restart plugin_loader.service
